b7a5c9dd21499426d71a6e22e281d86fda2aff1e
igorpilot <ihorzhurba.air@gmail.com>
2025-01-09 11:59:35 +0100

    Updates

--- ShopData-win32-x64/resources/app/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Імпортуйте з 'react-dom/client'
-import App from './App';
-
-// Використовуємо createRoot замість render
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
